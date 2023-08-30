f2b1b951893e172438e695160513fd3026cc6671
QuangAnh29 <dinhquang02092000@gmail.com>
2023-08-30 17:03:10 +0700

    TTAo project

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+     public float speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/light_sen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light_sen : MonoBehaviour
+{
+    public Light my_L;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player") 
+        {
+            my_L.enabled = true;
+        }
+    }
+
+}
